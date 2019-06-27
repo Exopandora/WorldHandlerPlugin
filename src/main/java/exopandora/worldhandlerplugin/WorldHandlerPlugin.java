a0de0ecc7664a557dc982a53c8c9b99af389beaa
Marcel Konrad <mkonrad98@alice-dsl.net>
2019-06-27 13:24:01 +0200

    Initial commit

---
+++ src/main/java/exopandora/worldhandlerplugin/WorldHandlerPlugin.java
@@ -0,0 +1,42 @@
+package exopandora.worldhandlerplugin;
+
+import exopandora.worldhandler.gui.category.Categories;
+import exopandora.worldhandler.gui.category.Category;
+import exopandora.worldhandler.gui.content.Content;
+import exopandora.worldhandler.helper.RegistryHelper;
+import exopandora.worldhandlerplugin.content.ContentExample;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(WorldHandlerPlugin.MODID)
+public class WorldHandlerPlugin
+{
+	public static final String MODID = "worldhandlerplugin";
+	public static final Content EXAMPLE = new ContentExample();
+	
+	public WorldHandlerPlugin()
+	{
+		// Register world handler specific events
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addGenericListener(Content.class, this::registerContent);
+		modEventBus.addGenericListener(Category.class, this::registerCategory);
+	}
+	
+	@SubscribeEvent
+	public void registerContent(Register<Content> event)
+	{
+		// Register our custom content
+		RegistryHelper.register(event.getRegistry(), WorldHandlerPlugin.MODID, "example", WorldHandlerPlugin.EXAMPLE);
+	}
+	
+	@SubscribeEvent
+	public void registerCategory(Register<Category> event)
+	{
+		// Replace containers tab with the example tab
+		Categories.MAIN.getContents().set(1, new ResourceLocation(WorldHandlerPlugin.MODID, "example"));
+	}
+}
---
+++ src/main/java/exopandora/worldhandlerplugin/content/ContentExample.java
@@ -0,0 +1,57 @@
+package exopandora.worldhandlerplugin.content;
+
+import exopandora.worldhandler.gui.button.GuiButtonBase;
+import exopandora.worldhandler.gui.category.Categories;
+import exopandora.worldhandler.gui.category.Category;
+import exopandora.worldhandler.gui.container.Container;
+import exopandora.worldhandler.gui.content.Content;
+import exopandora.worldhandler.helper.ActionHelper;
+import exopandora.worldhandlerplugin.WorldHandlerPlugin;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class ContentExample extends Content
+{
+	@Override
+	public void initButtons(Container container, int x, int y)
+	{
+		// Register default back and back to game buttons
+		
+		container.add(new GuiButtonBase(x, y + 96, 114, 20, I18n.format("gui.worldhandler.generic.back"), () -> ActionHelper.back(this)));
+		container.add(new GuiButtonBase(x + 118, y + 96, 114, 20, I18n.format("gui.worldhandler.generic.backToGame"), ActionHelper::backToGame));
+		
+		// Register custom buttons
+		
+		container.add(new GuiButtonBase(x, y, 232, 20, "Example button", () ->
+		{
+			Minecraft.getInstance().player.sendChatMessage("Example chat messsage");
+		}));
+	}
+	
+	@Override
+	public Category getCategory()
+	{
+		return Categories.MAIN;
+	}
+	
+	@Override
+	public String getTitle()
+	{
+		return "Example Title";
+	}
+	
+	@Override
+	public String getTabTitle()
+	{
+		return "Example";
+	}
+	
+	@Override
+	public Content getActiveContent()
+	{
+		return WorldHandlerPlugin.EXAMPLE;
+	}
+}
